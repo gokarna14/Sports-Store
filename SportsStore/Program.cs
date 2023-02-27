d8dce8baeaafc4593fda610cf605a4e63d27b77d
System Administrator <gokarna.adhikari14@gmail.com>
2023-02-27 22:34:37 +0545

    Cached rm

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -12,3 +12,7 @@ app.MapDefaultControllerRoute();
 
 // app.MapGet("/", () => "Hello World!");
 app.Run();
+
+
+
+// docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=yourStrong(!)Password" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2022-latest
